4bf5d17956ba10911d7acedb977dec509aa42113
Esther <eagyare91@gmail.com>
2021-03-11 02:00:26 +0000

    integration

---
+++ static/lib/api-music.js
@@ -0,0 +1,10 @@
+var spotifyClientId = '9ebe8c83d033468b94a0cf6b96b306ee';
+var spotifyClientSecret = 'd80d1ea8d73540e8aae9f087eeac032b';
+
+var deezerAppId = '466162';
+var deezerSecretKey = 'e91968cee565f1247484ff40e82058ae';
+
+exports.spotifyClientId = spotifyClientId;
+exports.spotifyClientSecret = spotifyClientSecret;
+exports.deezerAppId = deezerAppId;
+exports.deezerSecretKey = deezerSecretKey;
