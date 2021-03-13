db409626c8f1fe978a30973c36ccd4b09d277720
Matt Tamer <m.tamer94@gmail.com>
2021-03-13 14:18:08 -0500

    Converting from our original design to something smaller for just spotify

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-# importing Flask class from flask package
-from flask import Flask
-
-# create instance of Flask class stored as "app" variable
-# name is the argument. Flask uses this value to identify the location of the package
-app = Flask(__name__)
-
-from app import routes
-
